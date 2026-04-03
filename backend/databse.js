daff72a243301f0da035f7ba2cf09e3692ae92f2
tousifk436-coder <tousifk436@gmail.comm>
2026-04-03 12:33:20 +0530

    add

--- backend/databse.js
+++ backend/databse.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(
-      "mongodb://127.0.0.1:27017/ecommerceweb?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.10"
+      "mongodb+srv://tousifk436_db_user:Tousif123@cluster.414fpqv.mongodb.net"
     );
     console.log("Connected to mongoDB");
   } catch (error) {
